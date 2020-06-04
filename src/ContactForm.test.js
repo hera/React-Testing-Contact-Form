5eab53909b178830b62dd2afb32a14a575510183
hera <a.heraimenka@gmail.com>
2020-06-04 15:35:52 -0700

    Add basic test file

---
+++ src/ContactForm.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { fireEvent, render } from "@testing-library/react";
+import ContactForm from "./components/ContactForm";
+
+describe('test the form', () => {
+    const wrapper = render(<ContactForm />);
+
+    it('First name', () => {
+        
+    });
+});
