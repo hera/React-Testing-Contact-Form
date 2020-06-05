bf82805f4255f412019a0591cef09852ac6f7490
hera <a.heraimenka@gmail.com>
2020-06-05 02:53:49 -0700

    Edit form test. Check for response and change character limit for firstName

--- src/tests/ContactForm.test.js
+++ src/tests/ContactForm.test.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { screen, fireEvent, render } from "@testing-library/react";
+import { screen, fireEvent, render, waitFor } from "@testing-library/react";
 import ContactForm from "../components/ContactForm";
 import { act } from "react-dom/test-utils";
 
 test('First name', async () => {
     await act(async () => {
-        const wrapper = render(<ContactForm />);
+        render(<ContactForm />);
 
-        const firstName = wrapper.getByLabelText(/first name/i);
-        const lastName = wrapper.getByLabelText(/last name/i);
-        const email = wrapper.getByLabelText(/email/i);
-        const message = wrapper.getByLabelText(/message/i);
+        const firstName = screen.getByLabelText(/first name/i);
+        const lastName = screen.getByLabelText(/last name/i);
+        const email = screen.getByLabelText(/email/i);
+        const message = screen.getByLabelText(/message/i);
         const submitButton = screen.getByText(/submit/i);
 
         fireEvent.change(firstName, {target: {value: "Adam"}});
@@ -18,6 +18,10 @@ test('First name', async () => {
         fireEvent.change(email, {target: {value: "example@example.com"}});
         fireEvent.change(message, {target: {value: "Hello, world"}});
         fireEvent.click(submitButton);
-        
+
+        await waitFor(() => {
+            const response = screen.getByText(/firstName/);
+            expect(response).toBeInTheDocument();
+        });
     });
 });
